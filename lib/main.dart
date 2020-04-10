66565b97bcf436fb75d55079769bc3e3e3ff4940
sbhaskar <sbhaskar@adobe.com>
2020-04-10 18:05:55 +0530

    Added a comment.

--- lib/main.dart
+++ lib/main.dart
@@ -48,6 +48,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
+      // Added a comment.
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
